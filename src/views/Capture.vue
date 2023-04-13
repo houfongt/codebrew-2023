7f56670e371e8c58c42a4f62d06649ad3998c887
Edward <edwardtang0611@gmail.com>
2023-04-14 04:13:47 +1000

    Add a viewfinder

--- src/views/Capture.vue
+++ src/views/Capture.vue
@@ -4,6 +4,11 @@
         <div class="absolute top-5 left-5 z-50">
             <p class="text-2xl text-white bg-black/50 rounded-xl py-2 px-4">Magic Meal</p>
         </div>
+        <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 3.75H6A2.25 2.25 0 003.75 6v1.5M16.5 3.75H18A2.25 2.25 0 0120.25 6v1.5m0 9V18A2.25 2.25 0 0118 20.25h-1.5m-9 0H6A2.25 2.25 0 013.75 18v-1.5M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+        </div>
         <div class="absolute bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <button class="rounded-full border-white border-solid border-8 p-4">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
