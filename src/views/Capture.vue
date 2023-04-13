65be73ab40abb89012917dc0a17b2a1382ecb69e
Edward <edwardtang0611@gmail.com>
2023-04-14 04:06:01 +1000

    bug fix

--- src/views/Capture.vue
+++ src/views/Capture.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="relative">
-        <video v-show="!isPhotoTaken" ref="camera" class="w-full block" autoplay playsinline></video>
+        <video v-show="!isPhotoTaken" ref="camera" class="w-full block" autoplay playsinline muted></video>
         <div class="absolute top-5 left-5 z-50">
             <p class="text-2xl text-white bg-black/50 rounded-xl py-2 px-4">Magic Meal</p>
         </div>
         <div class="absolute bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <button class="rounded-full border-white border-solid border-[16px] p-8">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-12 h-12">
+            <button class="rounded-full border-white border-solid border-8 p-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                 </svg>
             </button>
@@ -45,7 +45,7 @@ export default {
             const constraints = (window.constraints = {
 			    audio: false,
 				video: {
-                    facingMode: 'user'
+                    facingMode: 'environment'
                 }
 			});
 			navigator.mediaDevices.getUserMedia(constraints).then(stream => {
