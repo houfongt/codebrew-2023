6ab5a44ffd2192601f546ce7c93e8d7d746b3183
Edward <edwardtang0611@gmail.com>
2023-04-14 12:38:13 +1000

    bug fix

--- src/views/Capture.vue
+++ src/views/Capture.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="fixed w-full bottom-0" style="padding-top: calc(env(safe-area-inset-top) + 32px )">
-        <video v-show="!isPhotoTaken" ref="camera" class="w-full h-full md:w-full md:h-screen block" autoplay playsinline muted></video>
+        <video v-show="!isPhotoTaken" ref="camera" class="w-full md:w-full md:h-screen block" autoplay playsinline muted></video>
         <div class="absolute top-5 left-5 z-50">
             <p class="text-2xl text-white bg-black/50 rounded-xl py-2 px-4">Magic Meal</p>
         </div>
